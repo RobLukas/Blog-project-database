126c130b9eb1feea08b6dd78a8958e4d56042514
RobLukas <rjlukasik@wp.pl>
2018-03-19 17:32:12 +0100

    Rename folders, files pages -> post;

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var operationsSql = require('./src/scripts/operationsSql');
 
 var app = express();
@@ -6,10 +7,14 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 var pageRouter = require('./src/routes/pageRoutes');
+
+require('./src/scripts/operationsSql');
 
 app.use('/post', pageRouter);
 
--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var pageRouter = express.Router();
-var operationsSql = require('./src/scripts/operationsSql');
+var operationsSql = require('../scripts/operationsSql');
 
 pageRouter.get('/:id', (req, res) => {
     operationsSql.getPostByIdFromDatabase((post) => {
-        res.render('page', {post: post.recordset});
-    }
+        res.render('post', {post: post.recordset});
+    })
+    console.log(req.params.id);
 })
 
 module.exports = pageRouter;
